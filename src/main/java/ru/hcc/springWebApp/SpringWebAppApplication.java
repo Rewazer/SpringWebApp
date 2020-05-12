24b100ad96effa83b5ea79c87f18981c65b1150a
Rewazer <runner.px@gmail.com>
2020-05-12 22:09:47 +0500

    JPA Entities

---
+++ src/main/java/ru/hcc/springWebApp/SpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.hcc.springWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppApplication.class, args);
+	}
+
+}
